edeae9e4695063c0f60b4f80c1d2fe116ce74edf
Eyas1996 <eyas.hm7@gmail.com>
2023-08-19 16:25:16 +0200

    Handle Submit

--- 05-invesment-calculator/src/App.js
+++ 05-invesment-calculator/src/App.js
@@ -1,18 +1,18 @@
 import Header from "./components/Header/Header";
-import Form from "./components/UserInput/UserInput";
-import Table from "./components/ResultsTable/ResultsTable";
+import UserInput from "./components/UserInput/UserInput";
+import ResultsTable from "./components/ResultsTable/ResultsTable";
 
 function App() {
   return (
     <div>
       <Header />
 
-      <Form />
+      <UserInput />
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <Table />
+      <ResultsTable />
     </div>
   );
 }
--- 05-invesment-calculator/src/components/UserInput/UserInput.js
+++ 05-invesment-calculator/src/components/UserInput/UserInput.js
@@ -1,17 +1,41 @@
 import React from "react";
-
 import "./UserInput.css";
+
 const UserInput = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("SUBMIT");
+  };
+
+  const restHandler = () => {
+    console.log("REST");
+  };
+
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={(e) =>
+              inputChangeHandler("current-savings", e.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={(e) =>
+              inputChangeHandler("yearly-contribution", e.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -19,15 +43,25 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            onChange={(e) =>
+              inputChangeHandler("expected-return", e.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            onChange={(e) => inputChangeHandler("duration", e.target.value)}
+            type="number"
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button onClick={restHandler} type="reset" className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
