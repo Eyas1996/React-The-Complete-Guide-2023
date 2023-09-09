1725876b9df90f9acf5e08e6dae245fe08580164
Eyas1996 <eyas.hm7@gmail.com>
2023-09-09 13:56:46 +0200

    Liffing the State Up

--- 05-invesment-calculator/src/components/ResultsTable/ResultsTable.js
+++ 05-invesment-calculator/src/components/ResultsTable/ResultsTable.js
@@ -1,33 +1,6 @@
 import React from "react";
 import "./ResultsTable.css";
 const ResultsTable = () => {
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
-
-    const yearlyData = []; // per-year results
-
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-
-    // do something with yearlyData ...
-  };
-
   return (
     <table className="result">
       <thead>
@@ -37,7 +10,7 @@ const ResultsTable = () => {
           <th>Interest (Year)</th>
           <th>Total Interest</th>
           <th>Invested Capital</th>
-        </tr> 
+        </tr>
       </thead>
       <tbody>
         <tr>
