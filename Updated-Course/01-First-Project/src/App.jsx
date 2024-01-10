458612293f3691cf9a96fcba8c2a7f9a4457fdfa
Eyas1996 <eyas.hm7@gmail.com>
2024-01-10 12:15:52 +0100

    Outputting List Data Dynamically

--- Updated-Course/01-First-Project/src/App.jsx
+++ Updated-Course/01-First-Project/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept {...conceptItem} />
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
             {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
